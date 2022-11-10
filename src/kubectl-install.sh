5de952d332d366514ad3ba2ae3147566a53e81c3
Konstantin Magg <66994300+konstantin-magg@users.noreply.github.com>
2022-11-10 16:31:53 +0100

    Update kubectl-install.sh

--- src/kubectl-install.sh
+++ src/kubectl-install.sh
@@ -3,5 +3,3 @@ echo "  Reference: <https://kubernetes.io/docs/tasks/tools/install-kubectl-linux
 
 sudo snap install kubectl --classic
 kubectl version --client
-
-echo "kubectl installed. Check out <https://github.com/ahmetb/kubectl-aliases> for bash aliases."
