c334e2f35fbdef65db85496884625996096b8da5
Konstantin Magg <66994300+konstantin-magg@users.noreply.github.com>
2022-11-10 16:31:37 +0100

    Update kubectl-install.sh

--- src/kubectl-install.sh
+++ src/kubectl-install.sh
@@ -1,4 +1,5 @@
 echo "== INSTALL K8S TOOLING =="
+echo "  Reference: <https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-other-package-management>"
 
 sudo snap install kubectl --classic
 kubectl version --client
