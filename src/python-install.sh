0e7c8ebfc8463fef61078aa499cc9483dc525b5e
Konstantin Magg <66994300+konstantin-magg@users.noreply.github.com>
2023-04-09 22:49:20 +0200

    Install virtualenv

--- src/python-install.sh
+++ src/python-install.sh
@@ -1,9 +1,17 @@
 echo "== INSTALL PYTHON =="
 
+echo "=== APT INSTALL ==="
 sudo apt update
 sudo apt upgrade
 sudo apt install \
+  python-is-python3 \
   python3 \
-  python3-pip
+  python3-pip \
+  python3-virtualenv
 
-echo "=== DONE ==="
+echo "=== VERIFY VERSIONS ==="
+python --version
+pip --version
+virtualenv --version
+
+echo "== DONE =="
